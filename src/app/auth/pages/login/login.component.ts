b2d198ff45cff8d13406494339c306de90dfef0d
Pavdev3 <pnunfe@gmail.com>
2024-06-24 12:27:24 +0200

    estilando la loginpage

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 @Component({
   standalone: true,
   selector: 'login ',
   templateUrl: './login.component.html',
+  imports: [
+    InputTextModule,
+    FloatLabelModule,
+    PasswordModule,
+    DividerModule,
+    ButtonModule,
+  ],
 })
 export class LoginComponent {}
--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
 @Component({
   standalone: true,
   selector: 'list-page',
   templateUrl: './list.component.html',
+  imports: [CardModule],
 })
 export class ListPage {}
