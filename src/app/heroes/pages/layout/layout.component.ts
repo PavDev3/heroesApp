132cf34e1c8883d169c96fe03a29037ff06cdf2e
pablo <pnunfe@gmail.com>
2024-06-25 12:20:12 +0200

    refactor: Update layout component to include menu bar and logout button
    The layout component has been updated to include a menu bar with navigation links and a logout button. This improves the user experience and provides easier access to different sections of the application.

--- src/app/heroes/pages/layout/layout.component.ts
+++ src/app/heroes/pages/layout/layout.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { SidebarModule } from 'primeng/sidebar';
 @Component({
   standalone: true,
   selector: 'layout',
   templateUrl: './layout.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarModule, ButtonModule, MenubarModule],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  sidebarVisible: boolean = false;
+
+  items = [
+    {
+      label: 'Menu',
+      icon: 'pi pi-fw pi-home',
+    },
+    {
+      label: 'Heroes',
+      icon: 'pi pi-fw pi-users',
+      routerLink: '/heroes',
+    },
+    {
+      label: 'About',
+      icon: 'pi pi-fw pi-info-circle',
+      routerLink: '/about',
+    },
+  ];
+
+  login = [
+    {
+      label: 'Sign Out',
+      icon: 'pi pi-fw pi-sign-out',
+      routerLink: '/auth/login',
+    },
+  ];
+}
