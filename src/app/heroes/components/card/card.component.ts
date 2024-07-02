b6ba96743a7868a50cd08edbd6c54a45621f3185
pablo <pnunfe@gmail.com>
2024-07-02 12:56:45 +0200

    refactor: Add edit button to hero card component
    The hero card component has been updated to include an "Edit" button. This allows users to easily navigate to the edit page for a specific hero. The button is displayed in the footer section of the card component.

--- src/app/heroes/components/card/card.component.ts
+++ src/app/heroes/components/card/card.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ChipModule } from 'primeng/chip';
+import { DividerModule } from 'primeng/divider';
 import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   standalone: true,
   selector: 'hero-card',
   templateUrl: './card.component.html',
-  imports: [CardModule, ChipModule, CommonModule],
+  imports: [
+    CardModule,
+    ChipModule,
+    CommonModule,
+    DividerModule,
+    ButtonModule,
+    RouterLink,
+  ],
 })
 export class CardComponent implements OnInit {
   @Input() hero!: Hero[];
