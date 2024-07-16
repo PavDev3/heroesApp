744c85359924a53b6880d1282c1833ee702e84a0
pablo <pnunfe@gmail.com>
2024-07-16 11:53:14 +0200

    refactor: Add "ProgressSpinner" component for loading state in HeroePage
    The HeroePage component has been updated to include a "ProgressSpinner" component from the primeng library. This component is used to display a loading state while the hero data is being fetched. It improves the user experience by providing visual feedback during the loading process.

--- src/app/heroes/pages/hero/heroe.component.ts
+++ src/app/heroes/pages/hero/heroe.component.ts
@@ -1,6 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { ButtonModule } from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { delay, switchMap } from 'rxjs';
+import { HeroImagePipe } from '../../../pipes/heroImage.pipe';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -8,8 +12,13 @@ import { HeroesService } from '../../services/heroes.service';
   standalone: true,
   selector: 'HeroePage',
   templateUrl: './heroe.component.html',
+  imports: [CommonModule, ProgressSpinnerModule, HeroImagePipe, ButtonModule],
 })
 export class HeroePage implements OnInit {
+  back() {
+    this.router.navigateByUrl('/heroes');
+  }
+
   public hero?: Hero;
 
   constructor(
@@ -19,7 +28,10 @@ export class HeroePage implements OnInit {
   ) {}
   ngOnInit(): void {
     this.activateRouter.params
-      .pipe(switchMap(({ id }) => this.heroesService.getHeroesById(id)))
+      .pipe(
+        delay(500),
+        switchMap(({ id }) => this.heroesService.getHeroesById(id))
+      )
       .subscribe((hero) => {
         if (!hero) {
           return this.router.navigateByUrl('/heroes');
