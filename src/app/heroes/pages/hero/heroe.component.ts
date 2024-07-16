b8d64d9ca0a11bc91a529cca64d47ef002760004
pablo <pnunfe@gmail.com>
2024-07-16 11:11:27 +0200

    refactor: Add lazy loading for hero image in HeroePage component

--- src/app/heroes/pages/hero/heroe.component.ts
+++ src/app/heroes/pages/hero/heroe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
@@ -8,13 +10,22 @@ import { HeroesService } from '../../services/heroes.service';
   templateUrl: './heroe.component.html',
 })
 export class HeroePage implements OnInit {
+  public hero?: Hero;
+
   constructor(
     private heroesService: HeroesService,
-    private activateRouter: ActivatedRoute
+    private activateRouter: ActivatedRoute,
+    private router: Router
   ) {}
   ngOnInit(): void {
-    this.activateRouter.params.pipe().subscribe((params) => {
-      console.log({ params });
-    });
+    this.activateRouter.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroesById(id)))
+      .subscribe((hero) => {
+        if (!hero) {
+          return this.router.navigateByUrl('/heroes');
+        }
+        this.hero = hero;
+        return;
+      });
   }
 }
