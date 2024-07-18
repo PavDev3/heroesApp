97cc7de54e35a00a99bf5ad7b11ef4e0d1435445
pablo <pnunfe@gmail.com>
2024-07-18 13:03:21 +0200

    Autocomplete search form

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -2,7 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
+import { debounceTime, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
 @Component({
   standalone: true,
   selector: 'search-page',
@@ -11,4 +18,24 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
 })
 export class SearchPage {
   public searchInput = new FormControl('');
+
+  public suggestions: Hero[] = [];
+
+  constructor(private heroesService: HeroesService) {
+    this.searchInput.valueChanges
+      .pipe(
+        debounceTime(300),
+        switchMap((value) => this.heroesService.getSuggestions(value!))
+      )
+      .subscribe((heroes) => {
+        this.suggestions = heroes;
+      });
+  }
+
+  filterHero(event: AutoCompleteCompleteEvent) {
+    const query = event.query;
+    this.heroesService.getSuggestions(query).subscribe((heroes) => {
+      this.suggestions = heroes;
+    });
+  }
 }
